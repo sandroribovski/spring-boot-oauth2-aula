1ffffa502e8def37de4a224096ff455eb64099bc
Sandro Henrique Vaz Ribovski <sandro.ribovski@liggatelecom.com.br>
2025-08-29 16:36:18 -0300

    BCryptPassword

--- password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$wbqiiq7Thjh7n3X1uO6L0e5XHPRGAKmxHv.H27E3UpZSbUokVyC9q');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$wbqiiq7Thjh7n3X1uO6L0e5XHPRGAKmxHv.H27E3UpZSbUokVyC9q');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
