e61048a9fe5e1cee8d08eaa73d70d308dd650ba5
Your Name <you@example.com>
2025-09-09 21:51:05 -0300

    Minha versão local com ajustes em User, SecurityConfig e novos arquivos

---
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/Role.java
@@ -0,0 +1,93 @@
+package com.devsuperior.demo.entities;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private Long id;
+	private String authority;
+	
+	
+	
+	
+	public Role() {		
+		
+	}
+
+
+
+	public Role(Long id, String authority) {
+		super();
+		this.id = id;
+		this.authority = authority;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getAuthority() {
+		return authority;
+	}
+
+
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(authority);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(authority, other.authority);
+	}
+	
+	
+	
+	
+	
+	
+	
+}
